11f18a512e2d07ad6063da4e5031adec9ae9e68f
André Baldeweg <56736413+abaldeweg@users.noreply.github.com>
2025-03-29 19:23:46 +0000

    Refactor head configuration in App.vue and main.ts

--- packages/vue-ts/src/App.vue
+++ packages/vue-ts/src/App.vue
@@ -4,11 +4,6 @@ import { useHead } from '@unhead/vue'
 
 useHead({
   title: 'Home',
-  titleTemplate: '%s%separator%siteName',
-  templateParams: { separator: ' - ', siteName: 'js-monorepo-template' },
-  htmlAttrs: {
-    lang: navigator.language,
-  },
 })
 </script>
 
